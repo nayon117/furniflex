c299c1360f2a553b266699ddc81abc8dfdecb9f8
nayon117 <hasibul.nayon1@gmail.com>
2024-09-07 07:58:19 +0600

    install firebase and setup authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Route from './route/Route.jsx'
+import { AuthProvider } from './provider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={Route}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Route}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
